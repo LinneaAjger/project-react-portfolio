fc875c3577265ab866e0ff4a2d3d686d1f620b50
LinneaAjger <linnea.ajger@gmail.com>
2024-11-19 20:09:39 +0100

    project image added and layout in that gridrow is updated

--- src/components/SingleProject.js
+++ src/components/SingleProject.js
@@ -1,29 +1,45 @@
 import React from 'react'
 import styled from 'styled-components'
-import { CardBox, CardText } from 'components/globalStyling'
+import projectImg from './images/projectImg.png'
 
 export const Project = () => {
   return (
-    <CardBox selectedColor="#113840" textColor="#B4D3D9">
+    <ProjectBox>
       <StyledDiv>
-        <CardText>
+        <div>
           <h4>Project</h4>
           <h5>tags, tags tags</h5>
           <p>text about this project</p>
-        </CardText>
-        <CardText>
-          <p>Image of project</p>
-        </CardText>
+        </div>
+        <ImgBox />
       </StyledDiv>
-    </CardBox>
+    </ProjectBox>
   )
 }
 
+const ImgBox = styled.div`
+  background: linear-gradient(rgb(17,56,64,0), rgb(17,56,64,0), rgb(17,56,64,0.9), rgb(17,56,64,1)), url(${projectImg});
+  background-size: cover;
+  width: 70%;
+  border-radius: 10px;
+  opacity: 0.8;
+  box-shadow: rgba(137, 216, 216, 0.05) 0px -60px 60px 0px
+`
+
+const ProjectBox = styled.div`
+  display: flex;
+  gap: 50px;
+  background-color: #113840;
+  color: #B4D3D9;
+  min-height: 300px;
+
+`
+
 const StyledDiv = styled.div`
+  font-family:"Uncut Sans";
   width: 100%;
   display: flex;
-  font-family:"Uncut Sans";
-
+  justify-content: space-between;
 h4 {
   font-size: 30px;
   font-weight: 700;
@@ -31,5 +47,9 @@ h4 {
 
 h5 {
   font-style: italic;
+}
+
+img {
+  border-radius: 10px;
 }
 `
